051db844ca1d072f252b6316309b778273ee1920
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-03-07 15:08:21 -0300

    build: main files

---
+++ spec/DesenvolvendoMe_Algorithms_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+RSpec.describe DesenvolvendoMeAlgorithms do
+  it "has a version number" do
+    expect(DesenvolvendoMeAlgorithms::VERSION).not_to be nil
+  end
+
+end

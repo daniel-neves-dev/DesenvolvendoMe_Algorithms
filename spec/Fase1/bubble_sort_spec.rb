99e85d55f17b3d6ed47e324bd5eff716a7129f5b
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-03-07 20:38:51 -0300

    test: Bubble Sort

--- spec/Fase1/bubble_sort_spec.rb
+++ spec/Fase1/bubble_sort_spec.rb
@@ -1,8 +1,10 @@
 require_relative "../../lib/Fase1/class_bubble_sort"
 
 RSpec.describe BubbleSort do
-  sorting = BubbleSort.new([5, 1, 4, 2, 8])
+  sorting1 = BubbleSort.new([5, 1, 4, 2, 8])
+  sorting2 = BubbleSort.new([64, 34, 25, 12, 22, 11, 90])
   it "Is organized in order" do
-    expect(sorting.bubble_sort).to eq("Sorted array: [1, 2, 4, 5, 8]")
+    expect(sorting1.bubble_sort).to eq("Sorted array: [1, 2, 4, 5, 8]")
+    expect(sorting2.bubble_sort).to eq("Sorted array: [11, 12, 22, 25, 34, 64, 90]")
   end
 end

557f283a984183b8fff6584ccbb32f72cfe9ac11
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-03-07 21:45:22 -0300

    test: minimum sum

--- lib/Fase1/minimum_sum.rb
+++ lib/Fase1/minimum_sum.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "../Fase1/class_minimum_sum"
-puts "Type some numbers:"
+puts "Type some numbers separated by coma with no spaces between:"
 array = gets.chomp
 result = Minimum::Sum.new(array).minimum_sum
 puts "The minimum sum is #{result}."
--- spec/Fase1/minimum_sum_spec.rb
+++ spec/Fase1/minimum_sum_spec.rb
@@ -4,11 +4,11 @@ require_relative "../../lib/Fase1/class_minimum_sum"
 
 RSpec.describe Minimum::Sum do
   it "minimum sum first array" do
-    result = Minimum::Sum.new([2, 4, 1, 3]).minimum_sum
+    result = Minimum::Sum.new("2, 4, 1, 3").minimum_sum
     expect(result).to be(3)
   end
   it "minimum sum second array" do
-    result = Minimum::Sum.new([7, 2, 3, 4, 5, 6]).minimum_sum
+    result = Minimum::Sum.new("7, 2, 3, 4, 5, 6").minimum_sum
     expect(result).to be(10)
   end
 end

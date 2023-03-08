4ded25b4579fa448a4dc24d86baf0d4afcb0eacb
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-03-07 21:54:41 -0300

    test: minimum sum

--- lib/Fase1/minimum_sum.rb
+++ lib/Fase1/minimum_sum.rb
@@ -4,4 +4,4 @@ require_relative "../Fase1/class_minimum_sum"
 puts "Type some numbers separated by coma with no spaces between:"
 array = gets.chomp
 result = Minimum::Sum.new(array).minimum_sum
-puts "The minimum sum is #{result}."
+puts "The minimum sum is: #{result}."

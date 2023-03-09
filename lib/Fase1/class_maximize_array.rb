6fcc7642584c7ea744106ca6b6dd8f22dd98f45a
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-03-08 22:18:27 -0300

    test: maximize array

--- lib/Fase1/class_maximize_array.rb
+++ lib/Fase1/class_maximize_array.rb
@@ -1,24 +1,29 @@
+# frozen_string_literal: true
+
 module Maximize
   class MaximumSum
     attr_reader :array
     attr_accessor :number
 
     def initialize(array, number)
-      @array = array #split(",").map(&:to_i) Converts into a array of integers with coma
+      @array = array.split(",").map(&:to_i) #Converts into a array of integers with coma
       @number = number
     end
 
     def maximize_sum
       sum = 0
-      abs_array = array.map(&:abs).sort
-      (array.length).times do
-        abs_array.each_with_index do |element, index|
-          sum += element[index]
-        end
+      number.times do
+        position = array.min
+        index = array.index(position)
+        array[index] = -array[index]
       end
-      return sum
+      array.length.times do |i|
+        sum += array[i]
+      end
+      sum
     end
   end
+
 end
 
 
--- spec/Fase1/maximize_array_spec.rb
+++ spec/Fase1/maximize_array_spec.rb
@@ -1,8 +1,17 @@
+# frozen_string_literal: true
+
 require_relative "../../lib/Fase1/class_maximize_array"
 
 RSpec.describe Maximize::MaximumSum do
-  it "return maximum sum" do
-    max_sum = Maximize::MaximumSum.new([-2, 0, 5, -1, 2], 4)
-    expect(max_sum.maximize_sum).to eq(10)
+  context "Return maximum sum of an array" do
+    it "Equal 10" do
+      max_sum = Maximize::MaximumSum.new("-2, 0, 5, -1, 2", 4)
+      expect(max_sum.maximize_sum).to eq(10)
+    end
+
+    it "Equal 20" do
+      max_sum = Maximize::MaximumSum.new("9, 8, 8, 5", 3)
+      expect(max_sum.maximize_sum).to eq(20)
+    end
   end
 end

85c86b29e2c4b57f542d7455a9d4fcbf8f68cacf
daniel <daniel.oliveira.au@hotmail.com>
2023-03-13 19:46:10 -0300

    text: find maximum sum stack2

--- lib/Fase1/class_find_maximum.rb
+++ lib/Fase1/class_find_maximum.rb
@@ -9,6 +9,7 @@ module FindMaximum
     def max_sum
       top1, top2 = 0, 0
       sum1, sum2 = 0, 0
+
       #Finding the initial sum of each stack
       stack1.each do |i|
         sum1 += i
@@ -16,17 +17,27 @@ module FindMaximum
       stack2.each do |i|
         sum2 += i
       end
-      while true
+      while true do
         #If any stack is empty
         if top1 == stack1.length || top2 == stack2.length
           return 0
         end
+
         #If the sum of all stacks are the same
         if sum1 == sum2
           return sum1
         end
+
+        #Finding maximum sum and removing the top element
+        if sum1 > sum2
+          sum1 -= stack1[top1]
+          top1 += 1
+        elsif sum2 > sum1
+          sum2 -=stack2[top2]
+          top2 += 1
+        end
       end
-      return sum1, sum2
+      return [sum1, sum2]
     end
   end
 end
--- spec/Fase1/find_maximum_spec.rb
+++ spec/Fase1/find_maximum_spec.rb
@@ -3,13 +3,13 @@ require_relative "../../lib/Fase1/class_find_maximum"
 RSpec.describe FindMaximum::MaxSum do
   context "Stacks" do
     it "sum of stack1 " do
-      stack1 = FindMaximum::MaxSum.new([1, 2], [0, 0]).max_sum
-      expect(stack1).to eq([3, 0])
+      stack1 = FindMaximum::MaxSum.new([1, 2], [3]).max_sum
+      expect(stack1).to eq(3)
     end
 
     it "sum of stack2 " do
-      stack2 = FindMaximum::MaxSum.new([0, 0], [3, 4]).max_sum
-      expect(stack2).to eq([0, 7])
+      stack2 = FindMaximum::MaxSum.new([7], [3, 4]).max_sum
+      expect(stack2).to eq(7)
     end
 
     it "stack is empty" do

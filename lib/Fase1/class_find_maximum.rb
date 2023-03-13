bc1c4d08cb49f9095b47adac34e46874a4f4a8be
daniel <daniel.oliveira.au@hotmail.com>
2023-03-13 19:55:09 -0300

    text: find maximum sum stack3

--- lib/Fase1/class_find_maximum.rb
+++ lib/Fase1/class_find_maximum.rb
@@ -1,14 +1,15 @@
 module FindMaximum
   class MaxSum
-    attr_accessor :stack1, :stack2
-    def initialize(stack1, stack2)
+    attr_accessor :stack1, :stack2, :stack3
+    def initialize(stack1, stack2, stack3)
       @stack1 = stack1
       @stack2 = stack2
+      @stack3 = stack3
     end
 
     def max_sum
-      top1, top2 = 0, 0
-      sum1, sum2 = 0, 0
+      top1, top2, top3 = 0, 0, 0
+      sum1, sum2, sum3 = 0, 0, 0
 
       #Finding the initial sum of each stack
       stack1.each do |i|
@@ -17,27 +18,33 @@ module FindMaximum
       stack2.each do |i|
         sum2 += i
       end
+      stack3.each do |i|
+        sum3 += i
+      end
       while true do
         #If any stack is empty
-        if top1 == stack1.length || top2 == stack2.length
+        if top1 == stack1.length || top2 == stack2.length || top3 == stack3.length
           return 0
         end
 
         #If the sum of all stacks are the same
-        if sum1 == sum2
+        if sum1 == sum2 && sum1 == sum3
           return sum1
         end
 
         #Finding maximum sum and removing the top element
-        if sum1 > sum2
+        if sum1 > sum2 && sum1 > sum3
           sum1 -= stack1[top1]
           top1 += 1
-        elsif sum2 > sum1
-          sum2 -=stack2[top2]
+        elsif sum2 > sum1 && sum2 > sum3
+          sum2 -= stack2[top2]
           top2 += 1
+        elsif sum3 > sum1 && sum3 > sum2
+        sum3 -= stack3[top3]
+        top3 += 1
         end
       end
-      return [sum1, sum2]
+      return [sum1, sum2, sum3]
     end
   end
 end
--- spec/Fase1/find_maximum_spec.rb
+++ spec/Fase1/find_maximum_spec.rb
@@ -3,22 +3,27 @@ require_relative "../../lib/Fase1/class_find_maximum"
 RSpec.describe FindMaximum::MaxSum do
   context "Stacks" do
     it "sum of stack1 " do
-      stack1 = FindMaximum::MaxSum.new([1, 2], [3]).max_sum
+      stack1 = FindMaximum::MaxSum.new([1, 2], [3], [3]).max_sum
       expect(stack1).to eq(3)
     end
 
     it "sum of stack2 " do
-      stack2 = FindMaximum::MaxSum.new([7], [3, 4]).max_sum
+      stack2 = FindMaximum::MaxSum.new([7], [3, 4], [7]).max_sum
       expect(stack2).to eq(7)
     end
 
+    it "sum of stack3 " do
+      stack3 = FindMaximum::MaxSum.new([11], [11], [5, 6]).max_sum
+      expect(stack3).to eq(11)
+    end
+
     it "stack is empty" do
-      empyt_stack = FindMaximum::MaxSum.new([],[]).max_sum
-      expect(empyt_stack).to eq(0)
+      empty_stack = FindMaximum::MaxSum.new([],[],[]).max_sum
+      expect(empty_stack).to eq(0)
     end
 
     it "the sum of all stacks are equal" do
-      equal_sum = FindMaximum::MaxSum.new([1, 2],[1, 2]).max_sum
+      equal_sum = FindMaximum::MaxSum.new([1, 2],[1, 2], [1, 2]).max_sum
       expect(equal_sum).to eq(3)
     end
   end

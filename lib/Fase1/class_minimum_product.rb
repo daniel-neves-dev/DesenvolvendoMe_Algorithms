3553467f1225561e32afb051d60c9d16d0f77f98
daniel <daniel.oliveira.au@hotmail.com>
2023-03-14 17:28:52 -0300

    text: all zeros and no negative number

--- lib/Fase1/class_minimum_product.rb
+++ lib/Fase1/class_minimum_product.rb
@@ -12,32 +12,33 @@ module Minimum
       # If the array has only one value
       return array[0] if array.length == 1
 
-      max_negative = -1.0/0.0
-      min_positive = 1.0/0.0
+      max_negative = -1.0 / 0.0
+      min_positive = 1.0 / 0.0
       count_zeros = 0
       count_negatives = 0
       prod = 1
 
       array.each do |i|
-
-        #Skip zero values
+        # Skip zero values
         if i == 0
           count_zeros += 1
           next
         end
 
-        #Count negatives and track the most negative
+        # Count negatives and track the most negative
         if i < 0
           count_negatives += 1
           max_negative = [max_negative, i].max
         end
 
-        if i > 0
-          min_positive = [min_positive, i].min
-        end
+        min_positive = [min_positive, i].min if i > 0
 
+        prod *= 1
       end
-      return min_positive
+
+      # If there are all zeros or no negative number
+      return 0 if count_zeros == (array.length) || (count_zeros > 0 && count_negatives == 0)
+      # return prod
     end
   end
 end
--- spec/Fase1/minimum_product_spec.rb
+++ spec/Fase1/minimum_product_spec.rb
@@ -1,14 +1,14 @@
 require_relative '../../lib/Fase1/class_minimum_product'
 
 RSpec.describe Minimum::MinimumProduct do
-  it "the array has only one number" do
-    one_number = Minimum::MinimumProduct.new("5").minimum_product
-    expect(one_number).to be(5)
-  end
+  #it "the array has only one number" do
+  #  one_number = Minimum::MinimumProduct.new("5").minimum_product
+  #  expect(one_number).to be(5)
+  #end
 
-  it "minimum positive" do
-    minimum_positive = Minimum::MinimumProduct.new("4,3,2,5,6").minimum_product
-    expect(minimum_positive).to eq(2)
+  it "all zeros and no negative numbers" do
+    all_zeros = Minimum::MinimumProduct.new("0,0,0,0").minimum_product
+    expect(all_zeros).to eq(0)
   end
 end
 

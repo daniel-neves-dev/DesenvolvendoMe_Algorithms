ed85e20aed53d85b8ba4e3f88f5342de12bb169f
daniel <daniel.oliveira.au@hotmail.com>
2023-03-14 22:30:59 -0300

    test: reversal

--- lib/Fase1/class_reversal_array.rb
+++ lib/Fase1/class_reversal_array.rb
@@ -1,9 +1,27 @@
-module Rotation
-  class Reversal
-    attr_reader :array
+ module Reverse
+  class Rotation
+    attr_accessor :array, :k
 
-    def initialize(array)
+    def initialize(array, k)
       @array = array.split(",").map(&:to_i)
+      @k = k
+    end
+    def reverse_array(array, start, ending)
+      while start < ending
+        array[start], array[ending] = array[ending], array[start]
+        start += 1
+        ending -= 1
+      end
+    end
+    def right_rotate(array, k, n)
+      reverse_array(array, 0, (n - 1))
+      reverse_array(array, 0, (k - 1))
+      reverse_array(array, k, (n - 1))
+    end
+    def print_array
+      n = array.length
+      right_rotate(array, k, n)
+      k.times { return array }
     end
   end
-end
+ end
--- spec/Fase1/reversal_array_spec.rb
+++ spec/Fase1/reversal_array_spec.rb
@@ -1,1 +1,15 @@
 require_relative '../../lib/Fase1/class_reversal_array'
+
+RSpec.describe Reverse::Rotation do
+  it "rotation 3 times" do
+    array_numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10"
+    result = Reverse::Rotation.new(array_numbers, 3).print_array
+    expect(result).to eq([8, 9, 10, 1, 2, 3, 4, 5, 6, 7])
+  end
+
+  it "rotation 2 times" do
+    array_numbers = "121, 232, 33, 43 ,5"
+    result = Reverse::Rotation.new(array_numbers, 2).print_array
+    expect(result).to eq([43, 5, 121, 232, 33])
+  end
+end

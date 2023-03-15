38139067ae2b32d1faf67df11018dd1ed4aea746
daniel <daniel.oliveira.au@hotmail.com>
2023-03-15 17:58:53 -0300

    test: missing characters

--- spec/Fase1/missing_character_spec.rb
+++ spec/Fase1/missing_character_spec.rb
@@ -1,9 +1,15 @@
-require_relative '../../lib/Fase1/class_missing_character'
+# frozen_string_literal: true
+
+require_relative "../../lib/Fase1/class_missing_character"
 
 RSpec.describe Pangram::MissingCharacter do
-
-  it "missing characters" do
+  it "missing characters test 1" do
     missing_chars = Pangram::MissingCharacter.new("Welcome to geeksforgeeks").missing_characters
     expect(missing_chars).to eq("abdhijnpquvxyz")
+  end
+
+  it "missing characters test 2" do
+    missing_chars = Pangram::MissingCharacter.new("The quick brown fox jumps").missing_characters
+    expect(missing_chars).to eq("adglvyz")
   end
 end

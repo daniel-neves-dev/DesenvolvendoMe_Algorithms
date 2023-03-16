cfca31b296c073d15c6c4de0f68b509e88709928
daniel <daniel.oliveira.au@hotmail.com>
2023-03-16 17:07:52 -0300

    feat: input

--- lib/Fase1/Inputs/calc_age.rb
+++ lib/Fase1/Inputs/calc_age.rb
@@ -1,5 +1,13 @@
-# frozen_string_literal: true
+require_relative "../class_calc_age"
 
-puts Time.now.mon
-puts Time.now.mday
-puts Time.now.year
+puts "Type the current date considered: 07/12/2017 "
+
+puts "Type your birth day"
+birth_day = gets.chomp.to_i
+puts "Type your birth month"
+birth_month = gets.chomp.to_i
+puts "Type your birth year"
+birth_year = gets.chomp.to_i
+
+result = Age::CalcAge.new(birth_day, birth_month, birth_year).print_cal_age
+puts result
